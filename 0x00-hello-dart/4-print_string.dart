8ca1fbaf588395f7a59b04920a9241c70b6461cf
Crystal ByNet <cristalbynet@gmail.com>
2024-06-24 13:57:45 +0200

    delete comment

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,7 +1,3 @@
-// print 3 times a string stored in the variable str,
-// followed by its first 9 characters.
-
-
 void main() {
   String str = "Holberton School";
   print('${str * 3}\n${str.substring(0, 9)}\n');
