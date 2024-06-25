a20ffea173c74dd46f955752fb51808d6909c1b7
Crystal ByNet <cristalbynet@gmail.com>
2024-06-25 13:30:09 +0200

    4. Outer Inner

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,8 @@
+void outer(String name, String id) {
+    String inner() {
+        String firstName = name.split(" ")[0];
+        String lastName = name.split(" ")[1];
+        return('Hello Agent ${lastName[0]}.${firstName} id is ${id} ');
+    }
+    print(inner());
+}
