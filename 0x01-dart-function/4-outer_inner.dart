9ea7426b74b75fad40f466b46deb3df065bf159c
Crystal ByNet <cristalbynet@gmail.com>
2024-06-25 13:31:46 +0200

    added world "your" in return

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -2,7 +2,7 @@ void outer(String name, String id) {
     String inner() {
         String firstName = name.split(" ")[0];
         String lastName = name.split(" ")[1];
-        return('Hello Agent ${lastName[0]}.${firstName} id is ${id} ');
+        return('Hello Agent ${lastName[0]}.${firstName} your id is ${id}');
     }
     print(inner());
 }
