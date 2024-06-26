64202fabb7c5b70f89408e9592fc6649301c480f
Crystal ByNet <cristalbynet@gmail.com>
2024-06-26 20:25:57 +0200

    8. Longest Unique Substring

---
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -0,0 +1,22 @@
+String longestUniqueSubstring(String str) {
+  int maxLength = 0;
+  String longestSubstring = "";
+
+  for (int i = 0; i < str.length; i++) {
+    String currentSubstring = "";
+    for (int j = i + 1; j < str.length; j++) {
+      if (currentSubstring.contains(str[j])) {
+        break;
+      } else {
+        currentSubstring += str[j];
+      }
+    }
+
+    if (currentSubstring.length > maxLength) {
+      maxLength = currentSubstring.length;
+      longestSubstring = currentSubstring;
+      
+    }
+  }
+  return longestSubstring;
+}
