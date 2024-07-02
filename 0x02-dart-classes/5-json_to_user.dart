1725b6a99f648faa915b479272a72b5e77dc5ccb
Crystal ByNet <cristalbynet@gmail.com>
2024-07-02 09:21:35 +0200

    update class User

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -8,24 +8,24 @@ class User {
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson["id"],
-      name: userJson["name"],
-      age: userJson["age"],
-      height: userJson["height"]
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      "id": id,
-      "name": name,
-      "age": age,
-      "height": height
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
     };
   }
 
   @override
   String toString() {
-    return 'User{id: $id, name: $name, age: $age, height: $height}';
+    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
   }
 }
